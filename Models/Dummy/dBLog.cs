5c7fd0f8d3272d587c2070def61cbe9a7c40a2cf
Nicolás Socoró <nicolassocoro@hotmail.com>
2019-07-23 21:28:27 -0300

    EDIT: dBlog

--- Models/Dummy/dBLog.cs
+++ Models/Dummy/dBLog.cs
@@ -21,7 +21,7 @@ namespace NET_Core_Introduction.Models.Dummy
 
                 return new List<eBlog>
                 {
-                    new eBlog(1, "How to make your first ASP NET Core Application", "", new List<string>{ "microsoft", "asp", "net" }, auth1),
+                    new eBlog(1, "How to make your first ASP NET Core Application", "https://medium.com/@nicolassocoro/asp-net-core-working-with-docker-de90a6bf67e4", new List<string>{ "microsoft", "asp", "net" }, auth1),
                     new eBlog(2, "Little introduction example of ASP NET Core web site.", "https://github.com/nicosocoro/Introduction_ASPNET_Core", new List<string>{ "mvc", "asp", "core", "net", "github" }, auth1),
                     new eBlog(3, "Introduction to Docker containers", "https://www.docker.com/resources/what-container", new List<string>{ "docker", "container"}, auth2),
                     new eBlog(4, "Why choose Docker?", "https://www.docker.com/why-docker", new List<string>{ "easy", "docker", "lightweight" }, auth2),
